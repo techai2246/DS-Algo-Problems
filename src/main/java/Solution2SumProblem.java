a1fc9c0406bdf23ff6a03274402705c1f9998d28
rhl08120 <rahul.sweden123@gmail.com>
2026-01-06 08:39:24 +0530

    updated solution with order of n

--- src/main/java/Solution2SumProblem.java
+++ src/main/java/Solution2SumProblem.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Solution2SumProblem {
-    //Bruteforce Approach
+    //Bruteforce Approach complexity would order of nˆ2
     public int[] twoSum(int[] nums, int target) {
         int result[]= new int[2];
         for(int i=0; i<=nums.length-2;i++){
@@ -16,11 +22,33 @@ public class Solution2SumProblem {
         return result;
     }
 
+    //Optimal Approach complexity would order of n
+
+    public int[] twoSumOptimal(int[] nums, int target){
+        int[] result = new int[2];
+        //LinkedList<Integer> integerList = Arrays.stream(nums).boxed().collect(Collectors.toCollection(LinkedList::new));
+        ArrayList<Integer> integerList = new ArrayList<>();
+        for(int element: nums){
+            integerList.add(element);
+        }
+        for(int i=0;i<=integerList.size()-1;i++){
+            int element = integerList.get(i);
+            int complement = target - element;
+            if(integerList.contains(complement)){
+                result[1]=integerList.indexOf(element);
+                result[0]=integerList.lastIndexOf(complement);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Solution2SumProblem solution = new Solution2SumProblem();
         int[] nums = {1, 3, 5, -7, 6, -3};
         int target = 0;
         int[] result = solution.twoSum(nums, target);
+        int[] resultOptimal = solution.twoSumOptimal(nums, target);
         System.out.println("[" + result[0] + ", " + result[1] + "]");
+        System.out.println("[" + resultOptimal[0] + ", " + resultOptimal[1] + "]");
     }
 }
