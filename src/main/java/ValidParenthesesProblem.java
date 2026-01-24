b2a8fa39ec13dda2b21f5e154681bc08cee91f43
rhl08120 <rahul.sweden123@gmail.com>
2026-01-24 19:56:45 +0530

    updated Paranthesis

---
+++ src/main/java/ValidParenthesesProblem.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
+public class ValidParenthesesProblem {
+
+    private final static HashMap<Character, Character> parenthesesMap = new HashMap<>();
+    private final static List<Character> openingParentheses = List.of('(', '{', '[');
+    private final static List<Character> closingParentheses = List.of(')', '}', ']');
+
+    static {
+        parenthesesMap.put(')', '(');
+        parenthesesMap.put('}', '{');
+        parenthesesMap.put(']', '[');
+    }
+
+    public static void main(String[] args) {
+        String s = "{[](}";
+        ValidParenthesesProblem validParenthesesProblem = new ValidParenthesesProblem();
+        boolean result = validParenthesesProblem.isValid(s);
+        System.out.println("Is the string \"" + s + "\" valid? " + result);
+    }
+
+    private boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i =0; i < s.length();i++){
+            Character ch = s.charAt(i);
+            if(openingParentheses.contains(ch)){
+                stack.push(ch);
+            } else if(closingParentheses.contains(ch)){
+                if(stack.isEmpty() || !stack.pop().equals(parenthesesMap.get(ch))){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
